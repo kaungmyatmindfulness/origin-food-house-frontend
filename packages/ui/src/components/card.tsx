cd39f50033812229d2276b38981feaece13bb767
kraft <kaungmyat.mindfulness@gmail.com>
2025-03-25 04:22:26 +0700

    feat: tailwind setup

--- packages/ui/src/components/card.tsx
+++ packages/ui/src/components/card.tsx
@@ -47,11 +47,7 @@ function CardDescription({ className, ...props }: React.ComponentProps<"div">) {
 
 function CardContent({ className, ...props }: React.ComponentProps<"div">) {
 	return (
-		<div
-			data-slot="card-content"
-			className={cn("px-6", className)}
-			{...props}
-		/>
+		<div data-slot="card-content" className={cn("p-6", className)} {...props} />
 	);
 }
 
